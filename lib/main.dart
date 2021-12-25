e96773d0402d6239229d2f67c85d912b95f2756b
Lucas Viana <lucas.lvtorres@gmail.com>
2021-12-25 17:34:27 -0400

    Recuperando valores

--- lib/main.dart
+++ lib/main.dart
@@ -11,73 +11,80 @@ class byteBankApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: formularioTransferencia(), //criando nossa classe responsavel pelo widget de card
+        body:
+            formularioTransferencia(), //criando nossa classe responsavel pelo widget de card
       ), //Indicando uma tela para o material design abri / tela home
     );
   }
 }
 
 class formularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoValor = TextEditingController();
+  final TextEditingController _controladorCampoConta = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Jesus Cristo é o meu Senhor!")),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              style: TextStyle(
-                fontSize: 24.0
+        appBar: AppBar(title: Text("Jesus Cristo é o meu Senhor!")),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoConta,
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    labelText: "Número conta", hintText: "0000"),
+                keyboardType: TextInputType.number,
               ),
-              decoration: InputDecoration(
-                labelText: "Número conta",
-                hintText: "0000"
             ),
-            keyboardType: TextInputType.number,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              style: TextStyle(
-                fontSize: 24.0
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: _controladorCampoValor,
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                    icon: Icon(Icons.monetization_on),
+                    labelText: "Valor",
+                    hintText: "00.00"),
+                keyboardType: TextInputType.number,
               ),
-              decoration: InputDecoration(
-                icon: Icon(Icons.monetization_on),
-                labelText: "Valor",
-                hintText: "00.00"
             ),
-            keyboardType: TextInputType.number,
-            ),
-          ),
-
-          ElevatedButton(onPressed: (){}, child: Text("Salvar"))
-        ],
-      )
-    );
+            ElevatedButton(onPressed: () {
+              final int?  valor = int.tryParse(_controladorCampoValor.text);
+              final int?  conta = int.tryParse(_controladorCampoConta.text);
+              if(valor != null && conta != null){
+                dadosTransferencia(valor, conta);
+                debugPrint("$valor e $conta");
+              }
+            }, child: Text("Salvar"))
+          ],
+        ));
   }
 }
 
-class transferencia extends StatelessWidget{
+class transferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: Text("JESUS CRISTO É O SENHOR!"),), //Adicionando um appBar em nosso aplicativo
+      appBar: AppBar(
+        title: Text("JESUS CRISTO É O SENHOR!"),
+      ),
+      //Adicionando um appBar em nosso aplicativo
       body: Column(
         children: [
-          cardTransferencia(dadosTransferencia(100,2001)),
-          cardTransferencia(dadosTransferencia(400,2001)),
-          cardTransferencia(dadosTransferencia(200,2001)),
-          cardTransferencia(dadosTransferencia(300,2001)),
+          cardTransferencia(dadosTransferencia(100, 2001)),
+          cardTransferencia(dadosTransferencia(400, 2001)),
+          cardTransferencia(dadosTransferencia(200, 2001)),
+          cardTransferencia(dadosTransferencia(300, 2001)),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
+        onPressed: () {},
         child: Icon(Icons.add),
       ),
-    );//Corpo do nosso app
+    ); //Corpo do nosso app
   }
 }
 
@@ -89,13 +96,12 @@ class cardTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-   return Card(
-       child: ListTile(
-         leading: Icon(Icons.monetization_on),
-         title: Text(_transferenciaDados.valor.toString()),
-         subtitle: Text(_transferenciaDados.numeroConta.toString()),
-       )
-   );
+    return Card(
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferenciaDados.valor.toString()),
+      subtitle: Text(_transferenciaDados.numeroConta.toString()),
+    ));
   }
 }
 
@@ -104,4 +110,9 @@ class dadosTransferencia {
   final int numeroConta;
 
   dadosTransferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'dadosTransferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
